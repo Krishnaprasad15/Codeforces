44a97bd3c0bbfc699224d2a42476176b64f46a6f
krishna1500 <krishna16151@gmail.com>
2023-12-07 00:25:39 +0530

    CP

---
+++ 800/A_Magical_Sticks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n&1) cout<<(n+1)/2<<"\n";
+    else cout<<n/2<<"\n";
+}
+int main() {
+ int t;
+ cin>>t;
+ while(t--) solve();
+ return 0;
+}
