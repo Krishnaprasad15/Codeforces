48931633d8a91c2ee8f4ac7eb5e1e42465b59849
krishna1500 <krishna16151@gmail.com>
2023-12-26 23:50:49 +0530

    CP

--- 800/C_Cypher.cpp
+++ 800/C_Cypher.cpp
@@ -5,22 +5,20 @@ void solve(){
     cin>>n;
     vector<int> a(n);
     for(auto &it:a) cin>>it;
-    int mx= *max_element(a.begin(),a.end());
+    // int mx= *max_element(a.begin(),a.end());
     for(int i=0;i<n;i++){
         int k;
         cin>>k;
         string s;
         cin>>s;
         for(int j=0;j<k;j++){
-            if(a[i]==mx && s[j]=='D'){
-                a[i]=-1;
-            }else if(a[i]==0 && s[j]=='U'){
-                a[i]=mx+1;
-            }
             if(s[j]=='D'){
                 a[i]+=1;
             }else a[i]-=1;
+            if(a[i]<0) a[i]=9;
         }
+        if(a[i]<0) a[i]*=-1;
+        a[i]%=10;
     }
     for(auto it:a) cout<<it<<" ";
     cout<<"\n";
