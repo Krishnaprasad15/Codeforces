1aaeb7048cccef7b1463b13fc8df6988f724914d
krishna1500 <krishna16151@gmail.com>
2023-12-30 23:51:44 +0530

    CP

---
+++ 800/A_Technical_Support.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnt=0;
+    if(n==1) cout<<"NO";
+    else{
+        for(int i=0;i<n;i++){
+            if(s[i]=='Q') cnt++;
+            else cnt--;
+        }
+        if(cnt<=0) cout<<"YES";
+        else cout<<"NO";
+    }
+    cout<<"\n";
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
