83f062dd72a8026718f6dabb212a269cb8af7103
krishna1500 <krishna16151@gmail.com>
2024-01-03 23:42:51 +0530

    CP

---
+++ 800/A_Omkar_and_Completion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) cout<<1<<" ";
+    cout<<"\n";
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
