de8932886563781b5c5c9417f6c40bd1962af2fc
krishna1500 <krishna16151@gmail.com>
2024-01-31 23:08:02 +0530

    CP

---
+++ 800/A_Long_Comparison.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+ 
+void solve(){
+        long long x1, x2;
+		int p1, p2;
+		cin >> x1 >> p1 >> x2 >> p2;
+		int mn = min(p1, p2);
+		p1 -= mn;
+		p2 -= mn;
+		if (p1 >= 7)
+			cout << ">" << endl;
+		else if (p2 >= 7)
+			cout << "<" << endl;
+		else{
+			for (int i = 0; i < p1; ++i) x1 *= 10;
+			for (int i = 0; i < p2; ++i) x2 *= 10;
+			if (x1 < x2)
+				cout << "<" << endl;
+			else if (x1 > x2)
+				cout << ">" << endl;
+			else
+				cout << "=" << endl;
+        }
+}
+int main() {
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
