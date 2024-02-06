421c8ffcce5731999ab95c8aa2fd1c960c4edc97
krishna1500 <krishna16151@gmail.com>
2024-02-06 21:09:10 +0530

    CP

--- 800/A_Divide_and_Multiply.cpp
+++ 800/A_Divide_and_Multiply.cpp
@@ -1,35 +1,37 @@
-#include <bits/stdc++.h> 
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 typedef long long ll;
- 
-void solve(){
-    ll n;
-    cin>>n ;
-    ll a[n];
-    ll mx= LONG_MIN;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-        mx= max(mx,a[i]);
-    }
-    sort(a,a+n);
-    ll sum=0,cnt=0;
-    for(int i=0;i<n-1;i++){
-        if(a[i]%2==0) cnt+=(a[i]/2);
-    }
-    while(cnt--) a[n-1]*=2;
 
-    for(int i=0;i<n-1;i++){
-        if(a[i]%2==0) sum+=1;
-        else sum+=a[i];
-    }
-    sum+=a[n-1];
-    cout<<sum<<"\n";
-}
 int main() {
     ll t;
-    cin>>t;
-    while(t--){
-        solve();
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        cin >> n;
+        vector<ll> arr(n);
+        ll sum = 0;
+        ll cnt=0;
+        for (ll i = 0; i < n; i++) {
+            cin >> arr[i];
+            while(arr[i]%2==0){
+                arr[i]/=2;
+                cnt++;
+            }
+        }
+        
+     sort(arr.begin(), arr.end());
+       while(cnt--){
+            arr[n-1]*=2;
+        }
+        for (ll i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        cout << sum << endl;
     }
+
     return 0;
 }
