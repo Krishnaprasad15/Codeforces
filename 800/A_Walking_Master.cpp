a3b7fd142b005488a95692c465b4b01d866c9465
Krishna Prasad <krishna16151@gmail.com>
2024-03-19 23:08:02 +0530

    CP

---
+++ 800/A_Walking_Master.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "YES"<<nl
+#define NO cout << "NO"<<nl
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+void solve(){
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    if(b > d) cout<<-1<<nl;
+    else{
+        if((a+d-b)<c) cout<<-1<<nl;
+        else cout<< 2*(d-b)+a-c<<nl;
+    }
+}
+
+int main() {
+    optimize();
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
