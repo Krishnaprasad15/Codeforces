f98bb800e3514e8ebde7fb065fd0ef1d33722afb
Krishna Prasad <krishna16151@gmail.com>
2024-04-01 23:17:09 +0530

    CP

---
+++ 800/A_Are_You_a_Robot_Again.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef long long ll;
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "YES"<<nl
+#define NO cout << "NO"<<nl
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+void solve(){
+    cout<<"safety";
+}
+
+int main() {
+    optimize();
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
