2054f4a232acdf3c65e8e33cd8c9ef2c94f3b193
Krishna Prasad <krishna16151@gmail.com>
2024-04-03 23:20:15 +0530

    CP

---
+++ 800/A_Destroying_Bridges.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef long long ll;
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+#define YES cout << "YES"<<nl
+#define NO cout << "NO"<<nl
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+void solve(){
+    int a,b;
+    cin>>a >>b;
+    if(b>=(a-1)) cout<<1<<nl;
+    else cout<<a<<nl;
+}
+
+int main() {
+    optimize();
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
