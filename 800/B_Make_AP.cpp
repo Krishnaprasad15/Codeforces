c1efe77964234a2102ce713e42a7ae94e8a52dce
Krishna Prasad <krishna16151@gmail.com>
2024-04-12 23:42:31 +0530

    CP

--- 800/B_Make_AP.cpp
+++ 800/B_Make_AP.cpp
@@ -10,7 +10,13 @@ typedef long long ll;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
  
 void solve(){
-    
+    int a,b,c;
+    cin>>a >>b >>c;
+
+    if((2*b-c)>0 && (2*b-c)%a==0) YES;
+    else if((a+c)%(2*b)==0) YES;
+    else if((2*b-a)>0 && (2*b-a)%c==0) YES;
+    else NO;
 }
 
 int main() {
